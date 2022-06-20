b0a7a134cf94f758472c49bad9ba1e744fe1e5c1
Matyáš Laský <matyas.lasky.020@pslib.cz>
2022-06-20 15:32:10 +0200

    Přidejte soubory projektu.

---
+++ aritmetická posloupnost/aritmetická posloupnost.cpp
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int clen = 0;
+	int diference = 0;
+
+    printf("Zadej prvni clen posloupnosti: ");
+    scanf("%d", &clen);
+
+    printf("Urci rozdíl mezi cleny: ");
+    scanf("%d", &diference);
+
+    for (int i = 1; i <= 100; i++) {
+        printf("\nAritmeticka posloupnost je: %2d", clen);
+        clen += diference;
+    }
+}
+
